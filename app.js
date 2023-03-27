fba8b527d236842d60cdfe54e8a4dad723192efd
muhafash <me.muhfarash@gmail.com>
2023-03-27 15:35:24 +0700

    route dalam app

--- app.js
+++ app.js
@@ -1,52 +1,17 @@
-// const sallam = require("./module");//? Import 1 function
-const { sallam, fullName } = require("./module"); //? Import >1 function like an object
-// console.log(sallam(), "\n\t", fullName());
+//~ Import
+const express = require("express");
 
-const http = require("http"); //
-const moment = require("moment"); //
+//~ Initialize
+const app = express();
+const portServer = process.env.PORT ? process.env.PORT : 3001;
 
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/wellcome":
-      wellcome(res);
-      break;
-    case "/":
-      home(res);
-      break; //
-    default:
-      page404(res);
-      break;
-  }
+//~Routing
+app.use("/", (req, res, next) => {
+  res.send({
+    status: "Alhamdulillah Success!",
+    message: "Berhasil berjalan",
+  });
 });
 
-const wellcome = res => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/json");
-  res.write(
-    JSON.stringify({
-      status: "Alhamdulillah successfully",
-      message: "Welcome to MERN class week 8",
-      study: "Node JS",
-      loginAt: moment(),
-    })
-  );
-  res.end();
-};
-
-const page404 = res => {
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "text/json");
-  res.write(
-    JSON.stringify({
-      status: "Page not found",
-      message: "Please input a correct page",
-    })
-  );
-  res.end();
-};
-
-const home = res => {
-  res.end(`<h1>${sallam()}</h1>`);
-};
-
-server.listen(3000, () => console.log("Server listening on port at http://localhost:3000"));
+//~ Start server
+app.listen(portServer, () => console.log(`Server listening on http://localhost:${portServer}`));
