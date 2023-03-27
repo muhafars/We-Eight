14c2be2dee7943d10167e41421d344bebabf6263
muhafash <me.muhfarash@gmail.com>
2023-03-27 14:08:52 +0700

    Up 1st

--- app.js
+++ app.js
@@ -6,16 +6,48 @@ const portServer = process.env.PORT ? process.env.PORT : 5000;
 
 //~ Routing
 const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/greet":
+      greet(res);
+      break;
+    case "/":
+      home(res);
+      break;
+    default:
+      pageNon(res);
+      break;
+  }
+});
+
+const greet = res => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/json");
   res.write(
     JSON.stringify({
       status: "Alhamdulillah Succesfully",
       message: "Welcome to MERN Class",
+      study: "Node JS",
       loginAt: moment().format("DD/MM/YY"),
     })
   );
   res.end();
-});
+};
 
+const pageNon = res => {
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/json");
+  res.write(
+    JSON.stringify({
+      status: "Not Found",
+      message: "Resource not found",
+    })
+  );
+  res.end();
+};
+
+const home = res => {
+  res.end(`<h1>Ini Home Page</h1>`);
+};
+
+//! Start the server
 server.listen(portServer, () => console.log(`Port server listening on ${portServer}`)); //
--- module/index.js
+++
@@ -1,5 +0,0 @@
-const sallam = () => "Assalamualikum Warahmatullah Wabarakatuh  ";
-const fullName = () => "Muhammad Fajri Assidiq";
-
-// module.exports = sallam;//export 1 module
-module.exports = { sallam, fullName };
